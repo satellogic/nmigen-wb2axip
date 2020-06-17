3f6f08b8f4c0d15a0ebc94ee3b9d96247705077d
Andres Demski <andresdemski@gmail.com>
2020-06-17 10:58:49 -0300

    utils.py: get_instance_ports and add_verilog_file

---
+++ nmigen_wb2axip/utils.py
@@ -0,0 +1,24 @@
+from urllib import request
+from nmigen.hdl.rec import DIR_FANIN, DIR_FANOUT, Record
+
+
+def dir_prefix(d):
+    assert d in [DIR_FANIN, DIR_FANOUT]
+    return 'i_' if d == DIR_FANIN else 'o_'
+
+
+def get_ports_for_instance(rec, prefix=''):
+    assert isinstance(rec, Record)
+    directions = {k: d for k, (s, d) in rec.layout.fields.items()}
+    return {
+        dir_prefix(directions[name]) + prefix + name: signal
+        for name, signal in rec.fields.items()
+    }
+
+
+def add_verilog_file(plat, file_name):
+    URL_FMT = "https://raw.githubusercontent.com/ZipCPU/wb2axip/master/rtl/{}"
+    if file_name not in plat.extra_files:
+        url = URL_FMT.format(file_name)
+        content = request.urlopen(url).read()
+        plat.add_file(file_name, content)
